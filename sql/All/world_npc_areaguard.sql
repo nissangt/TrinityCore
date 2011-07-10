1b8383324bf4d7b523034945a78be31ec32d97ee
Az@zel <azazel.kon@gmail.com>
2011-07-10 22:22:39 +0600

    Script/NPC AreaGuard: fix sql typo

--- sql/All/world_npc_areaguard.sql
+++ sql/All/world_npc_areaguard.sql
@@ -24,7 +24,7 @@ INSERT INTO `creature_template` (`entry`,`difficulty_entry_1`,`difficulty_entry_
 `faction_A`,`faction_H`,`npcflag`,`speed_walk`,`scale`,`rank`,`mindmg`,`maxdmg`,`dmgschool`,`attackpower`,`dmg_multiplier`,`baseattacktime`,`rangeattacktime`,`unit_class`,`unit_flags`,`dynamicflags`,`family`,`trainer_type`,`trainer_spell`,`trainer_class`,`trainer_race`,`minrangedmg`,`maxrangedmg`,`rangedattackpower`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,`resistance1`,`resistance2`,`resistance3`,`resistance4`,`resistance5`,`resistance6`,`spell1`,`spell2`,`spell3`,`spell4`,`spell5`,`spell6`,`spell7`,`spell8`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,`InhabitType`,`Health_mod`,`Mana_mod`,`Armor_mod`,`RacialLeader`,`questItem1`,`questItem2`,`questItem3`,`questItem4`,`questItem5`,`questItem6`,`movementId`,`RegenHealth`,`equipment_id`,`mechanic_immune_mask`,`flags_extra`,`ScriptName`) VALUES
 (92015,0,0,0,0,0,10458,0,0,0,'Terminator','Area Guard',NULL,0,80,80,0,35,35,0,0.91,1,0,7,7,0,3,1,2000,2200,1,390,0,0,0,0,0,0,1.76,2.42,100,7,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'',0,7,1,1,1,0,0,0,0,0,0,0,0,1,0,0,32770,'npc_areaguard');
 
-DELETE FORM `trinity_string` WHERE `entry` BETWEEN 6700 AND 6716;
+DELETE FROM `trinity_string` WHERE `entry` BETWEEN 6700 AND 6716;
 INSERT INTO `trinity_string`(`entry`, `content_default`) VALUES
 (6700, 'All'),
 (6701, 'Alliance'),
